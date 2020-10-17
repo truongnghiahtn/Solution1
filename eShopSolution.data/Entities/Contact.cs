712a9e6f98cbd17f39fce2dae9feb653150fea87
truongnghiahtn <truongnghiahtn@gmail.com>
2020-10-17 13:11:39 +0700

    xác nhận

---
+++ eShopSolution.data/Entities/Contact.cs
@@ -0,0 +1,18 @@
+﻿using eShopSolution.data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.data.Entities
+{
+    public class Contact
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Email { set; get; }
+        public string PhoneNumber { set; get; }
+        public string Message { set; get; }
+        public Status Status { set; get; }
+
+    }
+}

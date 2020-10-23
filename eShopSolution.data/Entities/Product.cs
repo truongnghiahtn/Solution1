9d0d5fc272d07d352daf0b68773250e6f97cea29
truongnghiahtn <truongnghiahtn@gmail.com>
2020-10-23 08:44:45 +0700

    xac nhan

--- eShopSolution.data/Entities/Product.cs
+++ eShopSolution.data/Entities/Product.cs
@@ -25,5 +25,7 @@ namespace eShopSolution.data.Entities
         public List<Cart> Carts { get; set; }
 
         public List<ProductTranslation> ProductTranslations { get; set; }
+
+        public List<ProductImage> ProductImages { get; set; }
     }
 }
--- eShopSolution.data/Migrations/20201023013827_AddProductImageTable.Designer.cs
+++ eShopSolution.data/Migrations/20201023013827_AddProductImageTable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eShopSolution.data.EF;
 
 namespace eShopSolution.data.Migrations
 {
     [DbContext(typeof(EShopDbContext))]
-    partial class EShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201023013827_AddProductImageTable")]
+    partial class AddProductImageTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
